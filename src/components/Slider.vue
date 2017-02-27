74a5d4a3952999bf7130df1dfa4ec7b8288ecf5c
wuhanjun <824786827@qq.com>
2017-02-27 08:43:06 +0800

    photo

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -8,19 +8,19 @@
                         <p>百度新闻 / 文</p>
                     </div>
                 </li>
-                <li><img src="news2.jpg" alt=""/>
+                <li><img src="../assets/images/news2.jpg" alt=""/>
                     <div class="text-box">
                         <h2>苏州现山寨“伦敦塔桥” 四座塔楼气势恢宏</h2>
                         <p>百度新闻 / 文</p>
                     </div>
                 </li>
-                <li><img src="news3.jpg" alt=""/>
+                <li><img src="../assets/img/home.png" alt=""/>
                     <div class="text-box">
                         <h2>邓超揭秘为何白了头， 刘诗诗被赞有两条事业线</h2>
                         <p>百度新闻 / 文</p>
                     </div>
                 </li>
-                <li><img src="news4.jpg" alt=""/>
+                <li><img src="../assets/images/news4.jpg" alt=""/>
                     <div class="text-box">
                         <h2>木狼双雄难敌火箭八人上双 火箭142-130森林狼</h2>
                         <p>百度新闻 / 文</p>
